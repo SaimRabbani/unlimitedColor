876cb1da95ff9edbba7435828975c373dad22571
SaimRabbani <121453154+SaimRabbani@users.noreply.github.com>
2023-10-06 23:27:31 -0700

    Add files via upload
    files added

---
+++ script.js
@@ -0,0 +1,28 @@
+// generate a random color
+
+const randomColor = function () {
+  const hex = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+let intervalId;
+const startChangeingColor = function () {
+  if (!intervalId) {
+    intervalId = setInterval(changeByColor, 2000);
+    
+  }
+  function changeByColor() {
+    document.body.style.backgroundColor = randomColor();
+  }
+};
+
+const stopChangeingColor = function () {
+  clearInterval(intervalId);
+  intervalId = null;
+};
+
+document.querySelector("#start").addEventListener("click", startChangeingColor);
+document.querySelector("#stop").addEventListener("click", stopChangeingColor);
